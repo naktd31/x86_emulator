112e3f474d49378297ff288ef2608f26aeaad731
naktd31 <32568348+naktd31@users.noreply.github.com>
2021-06-28 04:43:18 +0900

    update operations.c/.h

--- operations.c
+++ operations.c
@@ -8,6 +8,90 @@ void mov_r32_imm32(Emulator* emu)
   emu->eip += 5;
 }
 
+void mov_rm32_imm32(Emulator* emu)
+{
+  emu->eip += 1;
+  ModRM modrm;
+  parse_modrm(emu, &modrm);
+  uint32_t value = get_code32(emu, 0);
+  emu->eip += 4;
+  set_rm32(emu, &modrm, value);
+}
+
+void mov_rm32_r32(Emulator* emu)
+{
+  emu->eip += 1;
+  ModRM modrm;
+  parse_modrm(emu, &modrm);
+  uint32_t r32 = get_r32(emu, &modrm);
+  set_rm32(emu, &modrm, r32);
+}
+
+void mov_r32_rm32(Emulator* emu)
+{
+  emu->eip += 1;
+  ModRM modrm;
+  parse_modrm(emu, &modrm);
+  uint32_t rm32 = get_rm32(emu, &modrm);
+  set_r32(emu, &modrm, rm32);
+}
+
+void add_rm32_r32(Emulator* emu)
+{
+  emu->eip += 1;
+  ModRM modrm;
+  parse_modrm(emu, &modrm);
+  uint32_t r32 = get_r32(emu, &modrm);
+  uint32_t rm32 = get_rm32(emu, &modrm);
+  set_rm32(emu, &modrm, r32+rm32);
+}
+
+void sub_rm32_imm8(Emulator* emu, ModRM* modrm)
+{
+  uint32_t rm32 = get_rm32(emu, modrm);
+  uint8_t imm8 = get_sign_code8(emu, 0);
+  emu->eip += 1;
+  set_rm32(emu, modrm, rm32-imm8);
+}
+
+void code_83(Emulator* emu)
+{
+  emu->eip += 1;
+  ModRM modrm;
+  parse_modrm(emu, &modrm);
+
+  switch(modrm.opcode){
+    case 5:
+      sub_rm32_imm8(emu, &modrm);
+      break;
+    default:
+      printf("not implemented: 83 /%d\n",modrm.opcode);
+      exit(1);
+  }
+}
+
+void inc_rm32(Emulator* emu, ModRM* modrm)
+{
+  uint32_t value = get_rm32(emu, modrm);
+  set_rm32(emu, modrm, value+1);
+}
+
+void code_ff(Emulator* emu)
+{
+  emu->eip += 1;
+  ModRM modrm;
+  parse_modrm(emu, &modrm);
+
+  switch(modrm.opcode){
+    case 0:
+      inc_rm32(emu, &modrm);
+      break;
+    default:
+      printf("not implemented: FF /%d\n", modrm.opcode);
+      exit(1);
+  }
+}
+
 void short_jump(Emulator* emu)
 {
   int8_t diff = get_sign_code8(emu, 1);
@@ -25,9 +109,15 @@ void init_instructions(void)
 {
   int i;
   memset(instructions, 0, sizeof(instructions));
+  instructions[0x01] = add_rm32_r32;
+  instructions[0x83] = code_83;
+  instructions[0x89] = mov_rm32_r32;
+  instructions[0x8B] = mov_r32_rm32;
   for(i=0; i<8; i++){
     instructions[0xB8 + i] = mov_r32_imm32;
   }
+  instructions[0xC7] = mov_rm32_imm32;
   instructions[0xE9] = near_jump;
   instructions[0xEB] = short_jump;
+  instructions[0xFF] = code_ff;
 }
--- operations.h
+++ operations.h
@@ -2,12 +2,21 @@
 #define _OPERATIONS_H_
 
 #include "emulator.h"
-#include "get_code.h"
+//#include "get_code.h"
+#include "modrm.h"
 
 typedef void instruction_func_t(Emulator*);
 extern instruction_func_t* instructions[256];
 
 void mov_r32_imm32(Emulator* emu);
+void mov_rm32_imm32(Emulator* emu);
+void mov_rm32_r32(Emulator* emu);
+void mov_r32_rm32(Emulator* emu);
+void add_rm32_r32(Emulator* emu);
+void sub_rm32_imm8(Emulator* emu, ModRM* modrm);
+void code_83(Emulator* emu);
+void inc_rm32(Emulator* emu, ModRM* modrm);
+void code_ff(Emulator* emu);
 void short_jump(Emulator* emu);
 void near_jump(Emulator* emu);
 void init_instructions(void);
