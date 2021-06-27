cae140cf083a8d74c83f31259780679786ee0f32
naktd31 <32568348+naktd31@users.noreply.github.com>
2021-06-28 04:42:55 +0900

    update emulator.c/.h

--- emulator.c
+++ emulator.c
@@ -15,6 +15,69 @@ Emulator* create_emu(size_t size, uint32_t eip, uint32_t esp)
   return emu;
 }
 
+uint8_t get_code8(Emulator* emu, int index)
+{
+  return emu->memory[emu->eip + index];
+}
+
+int8_t get_sign_code8(Emulator* emu, int index)
+{
+  return (int8_t)emu->memory[emu->eip + index];
+}
+
+uint32_t get_code32(Emulator* emu, int index)
+{
+  int i;
+  uint32_t ret = 0;
+  for(i=0; i<4; i++){
+    ret |= get_code8(emu, index+i) << (i * 8);
+  }
+  return ret;
+}
+
+int32_t get_sign_code32(Emulator* emu, int index)
+{
+  return (int32_t)get_code32(emu, index);
+}
+
+void set_memory8(Emulator* emu, uint32_t address, uint8_t value)
+{
+  emu->memory[address] = value;
+}
+
+void set_memory32(Emulator* emu, uint32_t address, uint32_t value)
+{
+  int i;
+  for(i=0; i<4; i++){
+    set_memory8(emu, address+i, value>>(i*8));
+  }
+}
+
+uint32_t set_registers32(Emulator* emu, uint8_t index, uint32_t value)
+{
+  emu->registers[index] = value;
+}
+
+uint8_t get_memory8(Emulator* emu, uint32_t address)
+{
+  return emu->memory[address];
+}
+
+uint32_t get_memory32(Emulator* emu, uint32_t address)
+{
+  int i;
+  uint32_t ret = 0;
+  for(i=0; i<4; i++){
+    ret |= get_memory8(emu, address+i) << (8*i);
+  }
+  return ret;
+}
+
+uint32_t get_registers32(Emulator* emu, uint8_t index)
+{
+  return emu->registers[index];
+}
+
 void destroy_emu(Emulator* emu)
 {
   free(emu->memory);
--- emulator.h
+++ emulator.h
@@ -17,8 +17,21 @@ typedef struct {
   uint32_t eip;
 } Emulator;
 
+
 Emulator* create_emu(size_t size, uint32_t eip, uint32_t esp);
+uint8_t get_code8(Emulator* emu, int index);
+int8_t get_sign_code8(Emulator* emu, int index);
+uint32_t get_code32(Emulator* emu, int index);
+int32_t get_sign_code32(Emulator* emu, int index);
+void set_memory8(Emulator* emu, uint32_t address, uint8_t value);
+void set_memory32(Emulator* emu, uint32_t address, uint32_t value);
+uint32_t set_registers32(Emulator* emu, uint8_t index, uint32_t value);
+uint8_t get_memory8(Emulator* emu, uint32_t address);
+uint32_t get_memory32(Emulator* emu, uint32_t address);
+uint32_t get_registers32(Emulator* emu, uint8_t index);
 void destroy_emu(Emulator* emu);
 void dump_registers(Emulator* emu);
+
+#include "modrm.h"
 
 #endif
